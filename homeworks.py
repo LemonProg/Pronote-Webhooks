3fd66e6a52e6672ad2d78712e5311a71fd27a760
LEMON シ <87497636+LemonProg@users.noreply.github.com>
2023-02-26 13:29:29 +0100

    main files

---
+++ homeworks.py
@@ -0,0 +1,63 @@
+import pronotepy
+from pronotepy.ent import ent_var
+from datetime import datetime
+from datetime import timedelta
+import requests
+
+date = datetime.date(datetime.now()) # Date today
+date2 = date + timedelta(days=2) # Date in 2 days
+
+devoirs = "put your discord webhook"
+
+client = pronotepy.Client('put your direct link pronote url',
+                   username='...',
+                   password='...',
+                   ent=...)
+if client.logged_in:
+    homeworks = client.homework(date, date2)
+    
+    datas = {
+        "username": "Devoirs",
+        "embeds": [{
+            "title": f"Bonjour *{client.info.name}*",
+            "description": f"Vous avez **{len(homeworks)}** devoir(s)",
+        }],
+    }
+    x = requests.post(devoirs, json=datas)
+    
+    for work in homeworks:
+        def setDatas(description, color):
+            datas = {
+                "username": "Devoirs",
+                "embeds": [{
+                    "title": f"Matière : {work.subject.name}",
+                    "description": description,
+                    "color": color
+                }],
+            }
+
+            y = requests.post(devoirs, json=datas)
+        
+        if work.done == True:
+            couleur = "65280"
+            
+            timeLeft = work.date - date
+            timeLeft = str(timeLeft)[:2]
+            
+            desc = f"{work.description} \n\n Pour le {work.date} ({timeLeft} jours) \n État : Fait"
+            setDatas(desc, couleur)
+        else:
+            couleur = "16711680"
+            desc = f"{work.description} \n\n Pour le {work.date} ({timeLeft} jours) \n État : Non Fait"
+            setDatas(desc, couleur)
+        
+        for file in work.files:
+            datas = {
+                "username": "Devoirs",
+                "embeds": [{
+                    "description": f"[{file.name}]({file.url})",
+                }],
+            }
+            y = requests.post(devoirs, json=datas)        
+else:
+    print('no login')
---
+++ timeTable.py
@@ -0,0 +1,123 @@
+import pronotepy
+from pronotepy.ent import ent_var
+from datetime import datetime
+from datetime import timedelta
+import datetime as dt
+import requests
+
+date = datetime.date(datetime.now()) # Date today
+date2 = date + timedelta(days=3) # Date in 5 days
+
+timetable = "put your discord webhook"
+
+data = {
+    "username": "Emploie du temps",
+    "embeds": [{
+        "title": f"Journée du {date2}"
+    }]
+}
+
+x = requests.post(timetable, json=data)
+
+client = pronotepy.Client('put your direct link pronote url',
+                   username='...',
+                   password='...',
+                   ent=...)
+
+if client.logged_in:
+    lessons = client.lessons(date2)
+    
+    for lesson in lessons:
+        color = int(lesson.background_color.replace('#', ''), base=16)
+        hourStart = lesson.start.strftime('%H:%M')
+        hourEnd = lesson.end.strftime('%H:%M')
+        
+        datas = {
+            "username": "Emploie du temps",
+            "embeds": [{
+                "title": f"Matière : {lesson.subject.name}",
+                "description": f"Prof : {lesson.teacher_name} \nHoraires : {hourStart} / {hourEnd}",
+                "color": color
+            }],
+        }
+        if lesson.canceled:
+            datas = {}
+
+        y = requests.post(timetable, json=datas)
+    
+    menus = client.menus(date2)
+    
+    for menu in menus:
+        cantine = {
+            "username": "Cuisinier",
+            "avatar_url": "https://i.pinimg.com/originals/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.png",
+            "embeds": [{
+                'title': f'Repas du {menu.date}',
+            }]
+        }
+        z = requests.post(timetable, json=cantine)
+        
+        for first_meal in menu.first_meal:
+            cantine = {
+                "username": "Cuisinier",
+                "avatar_url": "https://i.pinimg.com/originals/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.png",
+                "embeds": [{
+                    'description': f'Entrée - {first_meal.name}',
+                }]
+            }
+            z = requests.post(timetable, json=cantine)
+            
+        for main_meal in menu.main_meal:
+            cantine = {
+                "username": "Cuisinier",
+                "avatar_url": "https://i.pinimg.com/originals/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.png",
+                "embeds": [{
+                    'description': f'Plat Principal - {main_meal.name}',
+                }]
+            }
+            z = requests.post(timetable, json=cantine)
+                
+        try:
+            for side_meal in menu.side_meal:
+                cantine = {
+                    "username": "Cuisinier",
+                    "avatar_url": "https://i.pinimg.com/originals/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.png",
+                    "embeds": [{
+                        'description': f'Second Plat - {side_meal.name}',
+                    }]
+                }
+                z = requests.post(timetable, json=cantine)
+        except TypeError:
+            pass
+                
+        for cheese in menu.cheese:
+            cantine = {
+                "username": "Cuisinier",
+                "avatar_url": "https://i.pinimg.com/originals/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.png",
+                "embeds": [{
+                    'description': f'Dessert/Fromage - {cheese.name}',
+                }]
+            }
+            z = requests.post(timetable, json=cantine)
+                    
+        for desert in menu.dessert:
+            cantine = {
+                "username": "Cuisinier",
+                "avatar_url": "https://i.pinimg.com/originals/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.png",
+                "embeds": [{
+                    'description': f'Dessert/Fromage - {desert.name}',
+                }]
+            }
+            z = requests.post(timetable, json=cantine)
+    
+    datas = {
+        "username": "Emploie du temps",
+        "embeds": [{
+            "title": f"Bonne journée *{client.info.name}*"
+        }]
+    }
+    
+    y = requests.post(timetable, json=datas)       
+else:
+    print('no login')
+
